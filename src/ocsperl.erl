458f5d6e0dca370ec9d28a0762a57e24a325b9bc
Mark Steele <mark@control-alt-del.org>
2016-01-15 13:47:22 -0500

    Updated to support dynamically loading code path based on env variable for escript

--- src/ocsperl.erl
+++ src/ocsperl.erl
@@ -10,19 +10,25 @@
 -on_load(init/0).
 
 init() ->
-  PrivDir = case code:priv_dir(?MODULE) of
-              {error, _} ->
-                EbinDir = filename:dirname(code:which(?MODULE)),
-                AppPath = filename:dirname(EbinDir),
-                filename:join(AppPath, "priv");
-              Path ->
-                Path
-            end,
-  erlang:load_nif(filename:join(PrivDir, "ocsperl"), 0).
+  Path = case application:get_env(code,sopath) of
+           {ok, CodePath} ->
+             CodePath;
+           _ ->
+             case code:priv_dir(?MODULE) of
+               {error, _} ->
+                 EbinDir = filename:dirname(code:which(?MODULE)),
+                 AppPath = filename:dirname(EbinDir),
+                 filename:join(AppPath, "priv");
+               CodePath ->
+                 CodePath
+             end
+         end,
+  erlang:load_nif(filename:join(Path, ?MODULE), 0).
 
--spec check(Cert::list(), CACert::list(), Issuer::list()) -> good | revoked | error.
-check(Cert, CACert, Issuer) ->
-  nif_check_ocsperl(Cert, CACert, Issuer).
+check(Cert, CACert, Issuer) when is_list(Cert), is_list(CACert), is_list(Issuer) ->
+  nif_check_ocsperl(Cert, CACert, Issuer);
+check(Cert, CACert, Issuer) when is_binary(Cert), is_binary(CACert), is_binary(Issuer) ->
+  check(binary_to_list(Cert), binary_to_list(CACert), binary_to_list(Issuer)).
 
 not_loaded(Line) ->
     erlang:nif_error({not_loaded, [{module, ?MODULE}, {line, Line}]}).
